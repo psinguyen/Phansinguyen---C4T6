ee3947305d60d3b187be7a9242dea1deefea4d36
psinguyen <42415217+psinguyen@users.noreply.github.com>
2018-10-14 15:57:00 +0700

    Add files via upload

---
+++ t.o.d.js
@@ -0,0 +1,6 @@
+var clicks = document.querySelectorAll(".click");
+var clicks = document.querySelectorAll(".click2");
+    clicks.addEventListener("click", () => {
+        console.log("clicked");
+
+    })
